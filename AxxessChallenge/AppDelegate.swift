67919d88153edcbc44a821cb587d72abf3b399aa
Nabil <nabilk@cdpindia.com>
2020-07-14 23:27:53 +0530

    First Commit

---
+++ AxxessChallenge/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  AxxessChallenge
+//
+//  Created by Nabil Kazi on 14/07/20.
+//  Copyright © 2020 Nabil Kazi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
