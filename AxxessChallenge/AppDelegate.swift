f49f6d8694fcce86b333b3d10b5ba38eb257a771
Nabil <nabilk@cdpindia.com>
2020-07-15 00:26:03 +0530

    Added Realm, SnapKit & Alamofire Pods

--- AxxessChallenge/AppDelegate.swift
+++ AxxessChallenge/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: ListVC())
         return true
     }
 }
